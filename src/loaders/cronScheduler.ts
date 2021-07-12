7c9dd613a50e5ceadaa3ca1d0dfb55a8ceb7d220
Diego Alloza <diego.allozagonzalez.practicas@telefonica.com>
2021-07-12 08:53:34 +0200

    refactor: set health check interval to 15 minutes

--- src/loaders/cronScheduler.ts
+++ src/loaders/cronScheduler.ts
@@ -1,7 +1,7 @@
 import schedule from "node-schedule";
 
 export default () => {
-  schedule.scheduleJob("* * * * *", function () {
-    console.log("Today is a new day!");
+  schedule.scheduleJob("*/15 * * * *", function () {
+    console.log("Health check");
   });
 };
