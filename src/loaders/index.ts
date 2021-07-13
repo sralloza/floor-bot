3927d8c6a21f1334c7e4302cb541cd1c2488ed3f
Diego Alloza <diego.allozagonzalez.practicas@telefonica.com>
2021-07-13 11:21:31 +0200

    feat: set up morgan

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,10 +1,13 @@
 import { Application } from "express";
+import morgan from "morgan";
 import dependencyInjector from "./dependencyInjector";
 import "./events";
 import expressLoader from "./express";
 import Logger from "./logger";
 
 export default async ({ expressApp }: { expressApp: Application }) => {
+  expressApp.use(morgan("combined"));
+
   await dependencyInjector();
   Logger.info("✌️ Dependency Injector loaded");
 
