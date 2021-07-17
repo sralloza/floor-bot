30a39c94046d2f2606db53622e9f597923b9831d
Diego Alloza González <sralloza@gmail.com>
2021-07-17 22:18:20 +0200

    chore: ensure entrypoint is using EOL=LF

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,7 @@
 #!/bin/sh
 set -e
 
-if [ -z "${DATABASE_HOST}" ]; then
-    echo "must set DATABASE_HOST"
-    exit 1
-fi
-wait-for-it.sh "${DATABASE_HOST}:3306"
+# This script exists just in case it's needed in the future.
+# Be aware of EOL=LF.
 
-echo "creating database"
-node ./createDatabase.js
-
-echo "migrations before"
-set +e
-typeorm migration:show
-set -e
-
-echo "running migrations"
-typeorm migration:run
-
-echo "migrations after"
-typeorm migration:show
-
-echo "starting server"
 node .
