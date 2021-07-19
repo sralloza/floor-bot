c786d7804816f6e02a7258c578c11148e2b59b65
Diego Alloza González <sralloza@gmail.com>
2021-07-19 23:18:46 +0200

    feat: add support for /completar_tarea bot command (#4)
    * feat: add command /completar_tarea
    
    * chore: remove reset task command

--- src/services/gsTasks.ts
+++ src/services/gsTasks.ts
@@ -22,6 +22,14 @@ interface AdvancedAssignedTask {
   kitchen: AdvancedSingleTask;
 }
 
+export type TaskType = "Bathroom" | "LivingRoom" | "Kitchen";
+
+interface UserTask {
+  week: number;
+  taskName: string;
+  taskType: TaskType;
+}
+
 interface DBInput {
   semana: number;
   baños: string;
@@ -54,6 +62,11 @@ export default class GSTasksService {
     cell.backgroundColor = { red: 1, green: 1, blue: 1, alpha: 1 };
   }
 
+  private getBackground(cell: GoogleSpreadsheetCell): "White" | "Green" {
+    const { done } = this.processSingleTaskCell(cell);
+    return done ? "Green" : "White";
+  }
+
   public async createAssignedTask(assignedTask: AssignedTask) {
     const sheet = this.doc.sheetsById[this.sheetID];
     const newRow: DBInput = {
@@ -63,6 +76,36 @@ export default class GSTasksService {
       cocina: assignedTask.kitchen,
     };
     await sheet.addRow(newRow as any);
+  }
+
+  public async getUserActiveAssignedTasks(
+    username: string
+  ): Promise<UserTask[]> {
+    const assignedTasks = await this.getAssignedTasks();
+    const filteredTasks: UserTask[] = [];
+
+    for (const task of assignedTasks) {
+      if (task.bathrooms.user === username && !task.bathrooms.done)
+        filteredTasks.push({
+          week: task.week,
+          taskName: "Baños",
+          taskType: "Bathroom",
+        });
+      if (task.livingRoom.user === username && !task.livingRoom.done)
+        filteredTasks.push({
+          week: task.week,
+          taskName: "Salón",
+          taskType: "LivingRoom",
+        });
+      if (task.kitchen.user === username && !task.kitchen.done)
+        filteredTasks.push({
+          week: task.week,
+          taskName: "Cocina",
+          taskType: "Kitchen",
+        });
+    }
+
+    return filteredTasks;
   }
 
   public async getAssignedTasks(): Promise<AdvancedAssignedTask[]> {
@@ -88,43 +131,43 @@ export default class GSTasksService {
     return data;
   }
 
-  public async finishTask(user: string): Promise<void> {
+  public async finishTask(
+    username: string,
+    week: number,
+    taskType: TaskType
+  ): Promise<void> {
     const sheet = this.doc.sheetsById[this.sheetID];
     const rows = await sheet.getRows();
+    let rowIndex = 0;
 
-    const rowIndex = rows.length;
+    for (let row of rows) {
+      rowIndex++;
+      if (+row.semana === week)
+        break;
+
+    }
+
     let row = rows[rowIndex - 1];
     await sheet.loadCells(row.a1Range.split("!")[1]);
 
-    const bathrooms = sheet.getCell(rowIndex, 1);
-    const livingRoom = sheet.getCell(rowIndex, 2);
-    const kitchen = sheet.getCell(rowIndex, 3);
+    let cell: GoogleSpreadsheetCell;
 
-    if (bathrooms.value == user) this.setGreenBackground(bathrooms);
-    else if (livingRoom.value == user) this.setGreenBackground(livingRoom);
-    else if (kitchen.value == user) this.setGreenBackground(kitchen);
-    else throw new Error("Invalid row (user not present)");
+    if (taskType === "Bathroom") cell = sheet.getCell(rowIndex, 1);
+    else if (taskType === "LivingRoom") cell = sheet.getCell(rowIndex, 2);
+    else cell = sheet.getCell(rowIndex, 3);
 
-    await sheet.saveUpdatedCells();
-  }
+    if (this.getBackground(cell) === "Green") {
+      console.error("Already finished");
+      console.error({ week, username, taskType, cell });
+      throw new Error("Already finised");
+    }
+    if (cell.value != username) {
+      console.error("Cell is not owned by user");
+      console.error({ week, username, taskType, cell });
+      throw new Error("Cell is not owned by user");
+    }
 
-  public async resetTask(user: string): Promise<void> {
-    const sheet = this.doc.sheetsById[this.sheetID];
-    const rows = await sheet.getRows();
-
-    const rowIndex = rows.length;
-    let row = rows[rowIndex - 1];
-    await sheet.loadCells(row.a1Range.split("!")[1]);
-
-    const bathrooms = sheet.getCell(rowIndex, 1);
-    const livingRoom = sheet.getCell(rowIndex, 2);
-    const kitchen = sheet.getCell(rowIndex, 3);
-
-    if (bathrooms.value == user) this.setWhiteBackground(bathrooms);
-    else if (livingRoom.value == user) this.setWhiteBackground(livingRoom);
-    else if (kitchen.value == user) this.setWhiteBackground(kitchen);
-    else throw new Error("Invalid row (user not present)");
-
+    this.setGreenBackground(cell);
     await sheet.saveUpdatedCells();
   }
 }
