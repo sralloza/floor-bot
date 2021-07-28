aafdc8baea15165790688fad41b128104539a686
Diego Alloza González <sralloza@gmail.com>
2021-07-28 19:48:41 +0200

    feat: add tickets command (#17)
    * chore: install axios and make-latex
    
    * chore: add types for make-latex
    
    * feat: implment tickets command
    
    * chore: bump version

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,12 +1,14 @@
 import { Telegraf } from "telegraf";
 import account from "./account";
 import tasks from "./tasks";
+import tickets from "./tickets";
 import transfer from "./transfer";
 import utils from "./utils";
 
 export default (bot: Telegraf): void => {
   account(bot);
   tasks(bot);
+  tickets(bot);
   transfer(bot);
   utils(bot);
 };
