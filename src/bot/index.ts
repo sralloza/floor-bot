bd91f79d06d901eaf2adb42a4a64f1e8ea321367
Diego Alloza <diego.allozagonzalez.practicas@telefonica.com>
2021-07-28 14:32:35 +0200

    refactor: remove subtasks

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,13 +1,11 @@
 import { Telegraf } from "telegraf";
 import account from "./account";
-import subtasks from "./subtasks";
 import tasks from "./tasks";
 import transfer from "./transfer";
 import utils from "./utils";
 
 export default (bot: Telegraf): void => {
   account(bot);
-  subtasks(bot);
   tasks(bot);
   transfer(bot);
   utils(bot);
