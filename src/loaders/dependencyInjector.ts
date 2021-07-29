73c91af8b9a50076bcda0ded3601179822d082aa
Diego Alloza González <diego.allozagonzalez.practicas@telefonica.com>
2021-07-29 13:48:04 +0200

    feat: add redis instance to typedi container

--- src/loaders/dependencyInjector.ts
+++ src/loaders/dependencyInjector.ts
@@ -1,3 +1,4 @@
+import IORedis from "ioredis";
 import { Container } from "typedi";
 import LoggerInstance from "./logger";
 
@@ -5,6 +6,9 @@ export default async (): Promise<void> => {
   try {
     Container.set("logger", LoggerInstance);
     LoggerInstance.info("Logger injected into container");
+
+    Container.set("redis", new IORedis());
+    LoggerInstance.info("Redis injected into container");
   } catch (e) {
     LoggerInstance.error("🔥 Error on dependency injector loader: %o", e);
     throw e;
