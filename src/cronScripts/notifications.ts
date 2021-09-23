418ee8aed5e140b1c31990bb13290878aa00a811
Diego Alloza González <sralloza@gmail.com>
2021-09-23 11:53:17 +0200

    chore: change notifications (#26)
    * chore: schedule tasks monday at 9 am
    
    * feat: add monday reminder
    
    * chore: lint
    
    * chore: bump version

--- src/cronScripts/notifications.ts
+++ src/cronScripts/notifications.ts
@@ -28,4 +28,30 @@ export default (): void => {
     }
     logger.debug("Sunday reminder finished");
   });
+
+  scheduleJob("monday-reminder", "30 8 * * 1", async () => {
+    const tasksService = Container.get(GSTasksService);
+    const usersService = Container.get(GSUsersService);
+    const logger: Logger = Container.get("logger");
+    const bot: Telegraf = Container.get("bot");
+
+    logger.debug("Firing monday reminder");
+    const users = await usersService.getUsers();
+
+    for (const user of users) {
+      const tasks = await tasksService.getUserRemainingTasks(user.username);
+      if (tasks.length && user.telegramID) {
+        const taskNames = tasks.map((e) => e.taskName).join(", ");
+        const msg =
+          `Ya es lunes y tienes tareas pendientes (${taskNames}). Si no puedes ` +
+          "completarlas, tranfiérelas a alguien que pueda.\nEn futuras versiones " +
+          "se efectuará la transferencia automáticamente el lunes por la noche " +
+          "si la tarea no se ha completado.";
+
+        await bot.telegram.sendMessage(user.telegramID, msg);
+        logger.info(`User ${user.username} notified for tasks '${taskNames}'`);
+      }
+    }
+    logger.debug("Monday reminder finished");
+  });
 };
