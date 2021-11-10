51be78181472b431c624d8e1e4ad4c6a36ea3efe
Diego Alloza González <sralloza@gmail.com>
2021-11-10 18:16:43 +0100

    chore: improve redis handling (#36)
    * feat: enable disabling redis
    
    * feat: better redis connection error handling
    
    * chore: bump version to 1.2.0
    
    * chore: remove redis env vars from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-export REDIS_HOST=${REDIS_HOST:=redis}
-export REDIS_PORT=${REDIS_PORT:=6379}
-
 # This script exists just in case it's needed in the future.
 # Be aware of EOL=LF.
 
