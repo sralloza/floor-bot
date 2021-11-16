3f8413516325e2bf4e3787474a73ea37fee8aa1d
Diego Alloza González <sralloza@gmail.com>
2021-11-16 14:47:23 +0100

    fix: cron jobs (#39)
    * chore: improve dockerfile
    
    * chore: bump version
    
    * fix: cron jobs

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,5 +15,17 @@ fi
 
 echo "Received '$1', assuming it's a cron"
 
-nodeCmd="require(\"./build/src/loaders/dependencyInjector\").default();require(\"./build/src/cronScripts\").$1Job()"
+nodeCmd="
+const jobsLoader = require(\"./build/src/loaders/jobsLoader\").default
+const cronScripts = require(\"./build/src/cronScripts\")
+const Logger = require(\"./build/src/loaders/logger\").default
+
+jobsLoader().then(() => {
+    Logger.info(\"Launching script\");
+    cronScripts.$1Job();
+  }
+);
+"
+
+echo "launching: $nodeCmd"
 node --unhandled-rejections=strict -e "$nodeCmd"
