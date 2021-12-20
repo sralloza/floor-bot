f95c31cf1ac1c886ad940f1aa63f37654b67cf31
Diego Alloza González <diego.allozagonzalez.practicas@telefonica.com>
2021-12-20 14:27:36 +0100

    chore: make sheet ids configurable via settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,8 +16,29 @@ if (!process.env.ADMIN_ID) {
   process.exit(1);
 }
 
+if (!process.env.SH_ID_USERS) {
+  console.error("Must set env var SH_ID_USERS");
+  process.exit(1);
+}
+if (!process.env.SH_ID_TASKS) {
+  console.error("Must set env var SH_ID_TASKS");
+  process.exit(1);
+}
+if (!process.env.SH_ID_TRANSACTIONS) {
+  console.error("Must set env var SH_ID_TRANSACTIONS");
+  process.exit(1);
+}
+if (!process.env.SH_ID_TICKETS) {
+  console.error("Must set env var SH_ID_TICKETS");
+  process.exit(1);
+}
+if (!process.env.SH_ID_LOGS) {
+  console.error("Must set env var SH_ID_LOGS");
+  process.exit(1);
+}
+
 const settings = {
-  admin_id: parseInt(process.env.ADMIN_ID || ""),
+  admin_id: parseInt(process.env.ADMIN_ID),
   api_prefix: "/",
   awaitTableGeneration: process.env.AWAIT_TABLE_GENERATION?.toLowerCase() === "true",
   enableCronIntegration: process.env.ENABLE_CRON_INTEGRATION?.toLowerCase() === "true",
@@ -26,18 +47,17 @@ const settings = {
   client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL as string,
   disableRedis: process.env.DISABLE_REDIS?.toLowerCase() === "true",
   google_sheets_ids: {
-    users: 2084291060,
-    tasks: 0,
-    exchangesRates: 1120508069,
-    transactions: 1169315892,
-    tickets: 1204432402,
-    logs: 1473685009
+    users: parseInt(process.env.SH_ID_USERS),
+    tasks: parseInt(process.env.SH_ID_TASKS),
+    transactions: parseInt(process.env.SH_ID_TRANSACTIONS),
+    tickets: parseInt(process.env.SH_ID_TICKETS),
+    logs: parseInt(process.env.SH_ID_LOGS)
   },
   logs_level: process.env.LOG_LEVEL || "silly",
-  port: parseInt(process.env.PORT || "80", 10),
+  port: parseInt(process.env.PORT || "80"),
   private_key: (process.env.GOOGLE_PRIVATE_KEY as string).replace(/\\n/gm, "\n"),
   redis_host: process.env.REDIS_HOST || "localhost",
-  redis_port: parseInt(process.env.REDIS_PORT || "6379", 10),
+  redis_port: parseInt(process.env.REDIS_PORT || "6379"),
   telegram_token_bot: process.env.TELEGRAM_TOKEN_BOT,
   disable_scheduler_start_date: process.env.DISABLE_SCHEDULER_START_DATE
     ? Date.parse(process.env.DISABLE_SCHEDULER_START_DATE)
