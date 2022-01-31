594a25e3a9ca200535eb8032162f8a8df4012751
Diego Alloza González <sralloza@gmail.com>
2022-01-31 11:07:24 +0100

    fix: new tasks depends on first task instead on last one (#62)
    * chore: fix redis library issues
    
    * fix: new tasks depends on first task instead on last one
    
    * chore: bump version to 1.12.2

--- src/services/arrays.ts
+++ src/services/arrays.ts
@@ -5,7 +5,7 @@ export default class ArraysService {
   constructor() {}
 
   public rotate<T>(array: T[], times = 1): T[] {
-    if (times < 0) throw new Error("Can't rotate negative times");
+    if (times < 0) times = array.length + times;
     while (times--) {
       const temp = array.shift() as T;
       array.push(temp);
