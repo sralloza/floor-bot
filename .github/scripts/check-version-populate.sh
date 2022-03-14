5c65e909e36212ab0c6af7044fbb6bb5b70a34af
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-03-14 16:48:02 +0100

    chore: uppercase workflow names

--- .github/scripts/check-version-populate.sh
+++ .github/scripts/check-version-populate.sh
@@ -7,11 +7,13 @@ lockVersion1=$(cat package-lock.json | jq -r '.version')
 lockVersion2=$(cat package-lock.json | jq -r '.packages | .[] | select(.name == "floor-bot") | .version')
 
 if [ "$officialVersion" != "$lockVersion1" ]; then
+  # XXX: show the error message in PR status instead of log
   echo "::error file=package-lock.json::Must update package-lock.json main version too, use 'npm install'."
   exit 1
 fi
 
 if [ "$officialVersion" != "$lockVersion2" ]; then
+  # XXX: show the error message in PR status instead of log
   echo "::error file=package-lock.json::Must update package-lock.json dependency version too, use 'npm install'."
   exit 1
 fi
